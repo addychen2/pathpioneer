7164b9dfa5feae38e674b7015f5ead5832d55192
anthony richards <anthony@anthonys-MacBook-Pro-2.local>
2024-01-02 17:06:46 -0800

    wow

--- pathpioneer/paths/prompt.js
+++ pathpioneer/paths/prompt.js
@@ -3,11 +3,13 @@ import { View } from 'react-native';
 import Button from '../components/button';
 import AddressInput from '../components/addressInput';
 import HierarchyConatiner from '../components/hierarchyContainer';
+import HeirarchyInput from '../components/setHierarchy';
 
 export default function Prompt(){
     return(
         <View>
         <HierarchyConatiner/>
+        <HeirarchyInput/>
         </View>
     );
 }
