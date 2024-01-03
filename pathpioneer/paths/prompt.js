8081a0b758a57ca653e5424f4a12e034d76a47c8
addychen2 <addisonxchen@gmail.com>
2024-01-02 17:05:37 -0800

    made hierarchy container

--- pathpioneer/paths/prompt.js
+++ pathpioneer/paths/prompt.js
@@ -3,11 +3,12 @@ import { View } from 'react-native';
 import Button from '../components/button';
 import AddressInput from '../components/addressInput';
 import HierarchyConatiner from '../components/hierarchyContainer';
+import MultiAddressContainer from '../components/multiaddresscontainer';
 
 export default function Prompt(){
     return(
         <View>
-        <HierarchyConatiner/>
+        <MultiAddressContainer/>
         </View>
     );
 }
